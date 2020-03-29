28a64c4b3975d496159212754809272feea84f9d
Anthony King <anthonydking@gmail.com>
2020-03-29 19:18:22 +0100

    Fix use of SafeConstructor.flatten_mapping with Round Trip
    SafeConstructor prepends the merged keys to the node value.
    
    This results in RoundTrip dropping the overridden value as part of its
    duplication check.
    
    Deduplicate the keys on our end to work around this problem.
    
    This also means we can allow duplication checks again

--- clean_actions/yaml.py
+++ clean_actions/yaml.py
@@ -1,5 +1,5 @@
 import io
-from typing import Iterable
+from typing import Iterable, cast
 
 import ruamel.yaml
 
@@ -8,10 +8,34 @@ def _yaml(*, output: io.StringIO = None) -> ruamel.yaml.YAML:
     yaml = ruamel.yaml.YAML(output=output)
 
     # Strip out aliases + anchors when dumping
+    yaml.constructor.flatten_mapping = _flatten_mapping(yaml.constructor)
     yaml.representer.ignore_aliases = lambda x: True
-    yaml.allow_duplicate_keys = True
-
     return yaml
+
+
+def _flatten_mapping(self):
+    def inner(node: ruamel.yaml.nodes.MappingNode):
+        ruamel.yaml.SafeConstructor.flatten_mapping(self, node)
+        # we're not looking at a merged node
+        if not getattr(node, "merge", None):
+            return None
+
+        merged = cast(list, node.merge)
+        # ruamel prepends the value, so remove them
+        original_value = node.value[len(merged) :]
+        original_value_keys = [key.value for key, _ in original_value]
+
+        # Add in the merge nodes, but only if they're not found on the original value.
+        new_value = [
+            (key, val) for key, val in merged if key.value not in original_value_keys
+        ] + original_value
+
+        node.value = new_value
+
+        del node.merge  # We're manually merging, so don't let ruamel see this.
+        return None
+
+    return inner
 
 
 def load(contents: str) -> dict:
