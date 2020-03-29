da916547d6eb1be3b3ff526c007208d7a0c7ba48
Anthony King <anthonydking@gmail.com>
2020-03-29 22:52:29 +0100

    add support for command environment variables

--- clean_actions/features/commands.py
+++ clean_actions/features/commands.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 def execute(obj: dict) -> None:
     for job in obj.get("jobs", {}).values():
         build_job(obj, job)
@@ -21,8 +24,15 @@ def build_job(obj: dict, job: dict) -> None:
     # in github world.
     step = job["steps"].pop(index)
 
+    step_env = step.get("env", {})
+    job_env = job.get("env", {})
+    global_env = obj.get("env", {})
+
     command_name = step["command"]
     command = get_command(obj, command_name)
+    populate_env_on_command(
+        command, step_env=step_env, global_env={**global_env, **job_env}
+    )
     command_steps = command.get("steps", [])
 
     # We go in reverse, as we're inserting at the same index each time.
@@ -44,4 +54,27 @@ def step_is_calling_a_command(step: dict) -> bool:
 
 def get_command(obj: dict, name: str) -> dict:
     command = obj["commands"][name]
-    return command
+    return deepcopy(command)
+
+
+def populate_env_on_command(command: dict, step_env: dict, global_env: dict) -> None:
+    for step in command.get("steps", []):
+        step.setdefault("env", {})
+
+        # the command environment is a default fallback. If a step defines
+        # a value already, ignore it
+        for key, val in command.get("env", {}).items():
+            step["env"].setdefault(key, val)
+
+        # global env contains the job env too, since it doesn't matter at this level.
+        # We should pop the keys if they exist, since they always overwrite the command
+        # scope.
+        for key in global_env:
+            step["env"].pop(key, None)
+
+        # Set the environment from the parent command call, which always overwrites.
+        step["env"].update(step_env)
+
+        # cleanup!
+        if not step["env"]:
+            step.pop("env")
